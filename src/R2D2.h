5e3fd532dc196cfd7bb3540ac16e71266920150a
astrea <astrea@autistici.org>
2017-11-16 11:40:03 +0100

    mod

--- src/R2D2.h
+++ src/R2D2.h
@@ -1,1 +1,11 @@
-jnbnnn
+//file header per le funzioni utilizzate nel main (per rendere il main piu' semplice)
+#ifndef R2D2_H_
+#define R2D2_H_
+
+#define A 3
+
+void syntax(int, char**);
+
+#endif
+
+/*END R2D2.h*/
